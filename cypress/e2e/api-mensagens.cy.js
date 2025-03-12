68399a8b319f1613f10a382e0945a82ce9217351
Enzo Dutra <enzo.dutra@retornar.com.br>
2025-03-12 10:59:39 -0300

    fix

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -3,7 +3,7 @@ describe('Api Adopet', ()=> {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/9861ed46-a8a6-4c36-b089-a374bf66bea8',
-            headers: Cypress.env()
+            headers: Cypress.env() // Coloque no arquivo cypress.env.json seu Bearer token
         }).then((res) => {
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty;
